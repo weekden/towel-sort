c90fcbabcdac75cd160629d06015ab600fa7fb05
DenisNedelko <dnsndlk@gmail.com>
2024-07-29 19:47:33 +0300

    feat: add solution writted earlei

--- src/index.js
+++ src/index.js
@@ -2,5 +2,18 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+    // Condition for returning an empty array
+    if (!matrix || matrix.length === 0) return []
+    // Condition for returning an not empty array. Method reduce with pahrametrs(acc item index)
+    /*calllback function
+    acc is accumulate for returning result
+    item is iterable item
+    index is index of iterable item 
+    concat - create new array and past into it elements current array*/
+    return matrix.reduce((acc, item, index) => {
+		if ((index+1) % 2 !== 0) {
+			acc = acc.concat(item)
+		} else acc = acc.concat(item.reverse())
+		return acc
+	});
 }
